422cd79323e141390272cf7bf86a2faa14a97eea
Saoki11111 <t.aoki11111@gmail.com>
2020-07-15 09:27:57 +0900

    example 内ではインスタンス変数は使わないほうがよいためsubjectを使う

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,16 +1,10 @@
 require 'calc'
 
 RSpec.describe Calc do
+  # before { @calc = Calc.new}
+  subject(:calc) { Calc.new }
   it {
-    calc = Calc.new
-    expect(calc.add(2, 3)).to eq(5) # eq ->matcher
-    expect(calc.add(2, 3)).not_to eq(5)
-    expect(calc.add(2, 3)).to true
-    expect(calc.add(2, 3)).to false
-    expect(calc.add(2, 3)).to be > 10
-    expect(calc.add(2, 3)).to be_between(1, 10).inclusive # 範囲
-    expect(calc).to respond_to(:add) # method があるか調べる
-    expect(calc.add(2, 3).integer?).to true
-    expect(calc.add(2, 3)).to be_integer # int か↑ と同じ
+    # calc = Calc.new
+    expect(calc.add(2, 3)).to eq(5)
   }
 end
