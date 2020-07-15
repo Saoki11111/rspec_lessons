2dae28f0ad0ceae900225d9046a7d275b6cf8bfe
Saoki11111 <t.aoki11111@gmail.com>
2020-07-15 09:32:09 +0900

    let で変数を使う

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -3,8 +3,13 @@ require 'calc'
 RSpec.describe Calc do
   # before { @calc = Calc.new}
   subject(:calc) { Calc.new }
-  it {
-    # calc = Calc.new
-    expect(calc.add(2, 3)).to eq(5)
-  }
+  # let 変数で管理
+  context "tax 5%" do
+    let(:tax) { 0.05 }
+    it { expect(calc.price(100, tax)).to eq(105) }
+  end
+  context "tax 8%" do
+    let(:tax) { 0.08 }
+    it { expect(calc.price(100, tax)).to eq(108) }
+  end
 end
