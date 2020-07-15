398491dc459b3a80b6aace2575526f1ded3a4864
Saoki11111 <t.aoki11111@gmail.com>
2020-07-15 09:12:47 +0900

    describe, context で処理のグループ分け

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,16 +1,16 @@
 require 'calc'
 
 RSpec.describe "A calc" do
-  # 共通化 :example -> 処理の開始前
-  #        :context -> 1回のみ
-  before(:example) do
-    @calc = Calc.new  
+  # テスト対象をグループ分けする
+  # describe(もの) or context(状況)
+  context "when normal mode" do
+    it "given 2 and 3, returns 5" do
+      calc = Calc.new
+      expect(calc.add(2, 3)).to eq(5)
+    end
   end
-  it "given 2 and 3, returns 5" do
-    expect(@calc.add(2, 3)).to eq(5)
-  end
-  # 三角測量
-  it "given 5 and 8, returns 13" do
-    expect(@calc.add(5, 8)).to eq(13)
+  context "when graph mode" do
+    it "draws grapht" do
+    end
   end
 end
