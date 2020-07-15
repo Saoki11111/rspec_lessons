b4506999cf9e7c9d104c99bae67d3b4348316265
Saoki11111 <t.aoki11111@gmail.com>
2020-07-15 09:22:19 +0900

    matcher のいろいろ

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,13 +1,16 @@
 require 'calc'
 
 RSpec.describe Calc do
-  # it / example / specify どれも同じ
-  # it -> 実行時 1 example, 0 failures と出ていた
-  # it do でも書ける
-  # it { } でも書ける
   it {
     calc = Calc.new
-    expect(calc.add(2, 3)).to eq(5)
+    expect(calc.add(2, 3)).to eq(5) # eq ->matcher
+    expect(calc.add(2, 3)).not_to eq(5)
+    expect(calc.add(2, 3)).to true
+    expect(calc.add(2, 3)).to false
+    expect(calc.add(2, 3)).to be > 10
+    expect(calc.add(2, 3)).to be_between(1, 10).inclusive # 範囲
+    expect(calc).to respond_to(:add) # method があるか調べる
+    expect(calc.add(2, 3).integer?).to true
+    expect(calc.add(2, 3)).to be_integer # int か↑ と同じ
   }
-  it "draws graph" # pending
 end
